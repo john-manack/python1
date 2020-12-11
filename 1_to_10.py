9fde1a7068e0ece598c1f72eea9ecf20f58b11dd
John Manack <jmnck28@gmail.com>
2020-12-11 13:38:19 -0500

    Adds additional problem files

---
+++ 1_to_10.py
@@ -0,0 +1,6 @@
+# Exercise Small - 7
+
+count = 1
+while count <= 10:
+    print(count)
+    count += 1
---
+++ n_to_m.py
@@ -0,0 +1,11 @@
+# Ex. Small - 8
+
+start = input("Start from: ")
+end = input("End on: ")
+
+count_start = int(start)
+count_end = int(end)
+
+while count_start <= count_end:
+    print(count_start)
+    count_start += 1
